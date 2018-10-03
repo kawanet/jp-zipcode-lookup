66db5608e9e344d980c5f59d4dc3b7060af3cf19
Yusuke Kawasaki <u-suke@kawa.net>
2018-10-04 02:14:10 +0900

    City.byZipcode() returns Array

--- test/90.invalid.ts
+++ test/90.invalid.ts
@@ -30,6 +30,30 @@ describe(FILENAME, () => {
         assert(!City.byCode(99999!));
     });
 
+    it('Pref.byCode()', () => {
+        assert.strictEqual(Pref.byZipcode(undefined!).length, 0);
+    });
+
+    it('Pref.byCode("XXXXXXX")', () => {
+        assert.strictEqual(Pref.byZipcode("XXXXXXX"!).length, 0);
+    });
+
+    it('Pref.byCode(9999999)', () => {
+        assert.strictEqual(Pref.byZipcode(9999999!).length, 0);
+    });
+
+    it('City.byCode()', () => {
+        assert.strictEqual(City.byZipcode(undefined!).length, 0);
+    });
+
+    it('City.byCode("XXXXXXX")', () => {
+        assert.strictEqual(City.byZipcode("XXXXXXX"!).length, 0);
+    });
+
+    it('City.byCode(9999999)', () => {
+        assert.strictEqual(City.byZipcode(9999999!).length, 0);
+    });
+
     it('Oaza.byCode()', () => {
         assert.strictEqual(Oaza.byZipcode(undefined!).length, 0);
     });
